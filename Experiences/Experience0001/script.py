d9e92e0cab4003b79f8780afe479c3b25142c54a
justalk <justal.kevin@gmail.com>
2023-02-17 20:23:08 +0000

    [FEATURE] Playing with the redirection

--- Experiences/Experience0001/script.py
+++ Experiences/Experience0001/script.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route("/experience0001/<int:id>")
+def experience0001Id(id):
+    return f"<p>Experience: {id}</p>"
+
+@app.route("/experience0001")
+def experience0001():
+    return "<p>Experience 0001</p>"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
